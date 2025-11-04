34ca503dcf1c9575716afbfd44a5f265a2b96ad2
fardanaljihad <fardanaljihad.18@gmail.com>
2025-11-04 20:24:53 +0700

    Init project and set up security

---
+++ src/test/java/com/collega/paymentgatewaycip/PaymentGatewayCipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collega.paymentgatewaycip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayCipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
